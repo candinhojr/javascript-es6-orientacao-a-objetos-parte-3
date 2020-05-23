209c0017f7495d6de9b6fbc8aa9cb80130249d64
candinhojr <candinholuiz@gmail.com>
2020-05-23 11:46:23 -0300

    simplify Ajax requests with the Fetch API: post()

--- client/js/app/services/HTTPService.js
+++ client/js/app/services/HTTPService.js
@@ -15,30 +15,11 @@ class HTTPService {
 
     post(url, dado) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-            /*
-            Não podemos enviar o objeto negociacao diretamente, precisamos convertê-lo para string, porque no protocolo HTTP os dados são transmitidos no formato texto. 
-            Veja que fizemos o contrário de quando recebemos os dados vindos do servidor. Lá, convertemos string em objeto.
-            */
-        });
-
+        return fetch(url, {
+            headers: { 'Content-type' : 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        }) 
+        .then(res => this._handleErros(res));
     }
 }
